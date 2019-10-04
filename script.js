633d01a8e41a8deeaeed3ee504b9294f3e91c120
bajrushivlora <bajrushivlora@gmail.com>
2019-10-04 11:26:49 -0400

    interactions

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var move = [5, 10, 15, 20];
 var randomMove = move[Math.floor(Math.random()*move.length)];
 
 
-for (var i = 0; i < 800; i++) {
+for (var i = 0; i < 900; i++) {
     var shapes = document.createElement('div');
     shapes.classList.add('shapeone');
     shapes.style = `transform: translateX(${randomMove}px);"`
@@ -28,30 +28,39 @@ for (var i = 0; i < 800; i++) {
 
 //I want each shape to have a randomMove value but it only chooses one random value from the array and assigns it for every shape.
 
+let circles = document.querySelectorAll('.shapeone');
 
-var circles = document.querySelectorAll('.shapeone');
-
-circles.forEach(function(shape, i) {
+circles.forEach(function(shapes, i) {
     var x = randomNumber(150, 250);
     var y = randomNumber(30, 100);
     var z = randomNumber(10, 50);
-    var width = [350, 360, 370, 380, 390];
+    var randomWidth = randomNumber(40, 1500);
     var height = [300, 400, 450, 500, 600];
-    var randomWidth = width[Math.floor(Math.random()*width.length)]; //select a random width from my width array
+    // var randomWidth = width[Math.floor(Math.random()*width.length)]; //select a random width from my width array
     var randomHeight = height[Math.floor(Math.random()*height.length)]; //select a random height from my height array    
 
-    shape.style.background = hslColor(x, y, z); 
-    shape.style.width = randomWidth + 'px';
-    shape.style.height = randomHeight + 'px';
+    shapes.style.background = hslColor(x, y, z); 
+    shapes.style.width = randomWidth + 'px';
+    shapes.style.height = randomHeight + 'px';
 });
 
-var appendToX = function(content, output){
-    document.getElementById(content).insertAdjacentHTML('beforeEnd', output);
-}
+circles.forEach(function(shapes, i) {
+    function decreaseWidth() {   
+        var currentWidth = parseFloat(shapes.style.width);
+        shapes.style.width = (currentWidth - 10) + "px";
+        }
 
-var outputString = function(location, string){
-    appendToX(location, string);
-}
+    setInterval(decreaseWidth, 100);
 
-setInterval(outputString, 300, "content", `<div> class="shapetwo" </div>`);
+    function someAction() {  
+        if (shapes.classList.contains('shape-interaction')) {
+            shapes.classList.remove('shape-interaction');
+          } else {
+            shapes.classList.add('shape-interaction');
+          }
 
+    circles.addEventListener("click", someAction);
+    
+    }
+    
+});
